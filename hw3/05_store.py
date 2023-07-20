6a01a3869fafda2f00090aa8c3df7640d9df56de
VP <john@smith.c>
2023-07-20 10:55:34 +0600

    2-мя методами

---
+++ hw3/05_store.py
@@ -0,0 +1,70 @@
+# Есть словарь кодов товаров
+
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+# Есть словарь списков количества товаров на складе.
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+# Рассчитать на какую сумму лежит каждого товара на складе.
+#
+# Вывести суммарную стоимость каждого товара на складе c помощью циклов
+# То есть: всего по лампам, стульям, етс.
+# Формат строки вывода: "<товар> - <кол-во> шт, стоимость <общая стоимость> руб"
+#
+# Алгоритм должен получиться приблизительно такой:
+#
+# цикл for по товарам с получением кода и названия товара
+#     инициализация переменных для подсчета количества и стоимости товара
+#     получение списка на складе по коду товара
+#     цикл for по списку на складе
+#         подсчет количества товара
+#         подсчет стоимости товара
+#     вывод на консоль количества и стоимости товара на складе
+
+goods_list = list(goods)
+#print(goods_list)
+#print(goods_list[0])
+#print(goods.keys())
+
+#print(len(goods))
+
+for key, value in goods.items():
+    #print(key)
+    #print(value)
+    store_value = store[value]
+    #print(store_value)
+    total_cost = 0
+    for i in range(len(store_value)):
+        #print(store_value[i])
+        total_cost += store_value[i]['quantity'] * store_value[i]['price']
+        #total_cost += total_cost
+    #print(total_cost)
+    #print(key)
+    print(f"total cost for item {key} is {total_cost}")
+
+        #print(store_value[i]['quantity'])
+    #print(store_value[0]['quantity'])
+
